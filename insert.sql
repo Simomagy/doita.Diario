6b292fc9f5ead684c1583cd8a9ac38b1902b7152
24Giugno <threedead@live.it>
2024-12-20 10:27:33 +0100

    Correzione virgola
    forza napoli

--- insert.sql
+++ insert.sql
@@ -1,5 +1,4 @@
 ﻿insert into Diario (DataScrittura, X, Y, Luogo, Descrizione)
-values (
-        '2000-01-01', 250.6, 649.74, 'Sconosciuto', 'Mi sono svegliato in una terra desolata. Sono solo e senza amici, temo l''arrivo dell''oscurità.' ),
-        ( '2000-01-02', 250.6, 649.74, 'Buco nel terreno', 'La notte è foriera di orrori oltre l''immaginazione. Al sorgere del sole il rumore del fuoco mi ha svegliato. Sono sopravvissuto come un vigliacco ma sono ancora vivo.',
-       );
+values
+('2000-01-01', 250.6, 649.74, 'Sconosciuto', 'Mi sono svegliato in una terra desolata. Sono solo e senza amici, temo l''arrivo dell''oscurità.' ),
+('2000-01-02', 250.6, 649.74, 'Buco nel terreno', 'La notte è foriera di orrori oltre l''immaginazione. Al sorgere del sole il rumore del fuoco mi ha svegliato. Sono sopravvissuto come un vigliacco ma sono ancora vivo.');
