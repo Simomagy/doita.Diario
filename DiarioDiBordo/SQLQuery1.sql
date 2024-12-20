7b4c3fad26b74e493ef752f9f1c783190113dcc5
MS Studios <simomagy02@gmail.com>
2024-12-20 16:44:27 +0100

    rimosso file inutile

--- DiarioDiBordo/SQLQuery1.sql
+++
@@ -1,4 +0,0 @@
-﻿insert into Diario (DataScrittura, X, Y, Luogo, Descrizione)
-values
-('2000-01-01', 0, 0, 'Sconosciuto', 'Mi sono svegliato in una terra desolata. Sono solo e senza amici. Temo l''arrivo dell''oscurità.' ),
-('2000-01-02', 21, 7, 'Buco nel terreno', 'La notte è foriera di orrori oltre l''immaginazione. Svegliato all''alba da rumore di fiamme. Sono sopravvissuto come un vigliacco, ma sono ancora vivo.');
