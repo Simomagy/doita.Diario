f64313a4f438f5d49e2788caa83b6b3cc7ff6ee4
24Giugno <threedead@live.it>
2024-12-20 12:09:35 +0100

    Aggiunta Pagina
    viva la fiken

---
+++ DiarioDiBordo/SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿insert into Diario (DataScrittura, X, Y, Luogo, Descrizione)
+values
+('2000-01-01', 0.00, 0.00, 'Sconosciuto', 'Mi sono svegliato in una terra desolata. Sono solo e senza amici. Temo l''arrivo dell''oscurità.' ),
+('2000-01-02', 21.5, 7.33, 'Buco nel terreno', 'La notte è foriera di orrori oltre l''immaginazione. Svegliato all''alba da rumore di fiamme. Sono sopravvissuto come un vigliacco, ma sono ancora vivo.');
